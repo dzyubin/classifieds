01c9f49c0abe9677c25366c37716528e49b12881
Vova Dzyubin <vovadzyubin@gmail.com>
2016-02-26 16:25:29 +0200

    Bootstraped app for Firebase deployment

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,18 @@
 angular
     .module("classifieds", ["ngMaterial", "ui.router", "firebase"])
-    .config(function ($mdThemingProvider, $stateProvider) {
+    .config(function ($mdThemingProvider, $stateProvider, $urlRouterProvider) {
         $mdThemingProvider.theme('default')
             .primaryPalette('blue-grey')
             .accentPalette('orange');
 
+        $urlRouterProvider.when('', '/classifieds');
+
         $stateProvider
+            .state('/', {
+                url: '/classifieds',
+                templateUrl: 'components/classifieds/classifieds.tpl.html',
+                controller: 'classifiedsCtrl as vm'
+            })
             .state('classifieds', {
                 url: '/classifieds',
                 templateUrl: 'components/classifieds/classifieds.tpl.html',
