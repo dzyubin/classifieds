dd81998aac1f016e176e8e9bf50914b13e3734ca
Vova Dzyubin <vovadzyubin@gmail.com>
2016-03-06 13:15:13 +0200

    add basic validation

--- components/classifieds/login/classifieds.login.ctr.js
+++ components/classifieds/login/classifieds.login.ctr.js
@@ -5,6 +5,11 @@
     angular
         .module('classifieds')
         .controller('loginClassifiedsCtrl', function ($scope, $state, $mdSidenav, $timeout, $mdDialog, classifiedsFactory) {
+
             this.sidenavOpen = true;
+
+            $scope.login = function () {
+                $scope.message = "Welcome " + $scope.user.email;
+            };
         });
 }());
